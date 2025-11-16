00bfe841f6e85bdaa1aa25a1fc98722671598ce2
HariGadi123 <hari@g10consultancy.com>
2025-11-16 10:14:54 +0530

    worked

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Light-Dark-Theme-Switcher/"   //repo name exactly
+  base:"/Light-Dark-Theme-Switcher-ContextAPI2/"   //repo name exactly
 })
